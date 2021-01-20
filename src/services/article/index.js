2cc3d57f70cc4a82ad376bbac6d61e8611715b4d
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-20 22:26:12 +0100

    adds another endpoint

--- src/services/article/index.js
+++ src/services/article/index.js
@@ -54,4 +54,19 @@ router.delete("/:id", async (req, res, next) => {
   }
 });
 
+router.get("/authorsAndCategory", async (req, res, next) => {
+  try {
+    const { rows } = await db.query(
+      `select a.content as article_content, au.name as author_name, c.name as cateogory_name
+      from articles as a inner join author as au inner join category as c
+      where a.id=au.id and c.id=a.id
+      `
+    );
+    res.send(rows);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+});
+
 module.exports = router;
